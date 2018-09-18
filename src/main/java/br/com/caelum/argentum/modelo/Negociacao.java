31b816445c9e7cb97243bbc2cd2c49bff130ab1d
osvaldo.lima <osvaldo.lima@ufba.br>
2018-09-18 14:39:43 -0300

    adding junit test classes

--- src/main/java/br/com/caelum/argentum/modelo/Negociacao.java
+++ src/main/java/br/com/caelum/argentum/modelo/Negociacao.java
@@ -8,11 +8,15 @@ public final class Negociacao {
 	private final int quantidade;
 	private final Calendar data;
 	
-	public Negociacao(double preco, int quantidade, Calendar data) {
+	public Negociacao(double preco, int quantidade, Calendar data) {		
 		super();
+		if (data == null) {
+			throw new IllegalArgumentException("data nao pode ser nula");
+			}
 		this.preco = preco;
 		this.quantidade = quantidade;
 		this.data = data;
+		
 	}
 	
 	public double getPreco() {
@@ -22,15 +26,14 @@ public final class Negociacao {
 	public int getQuantidade() {
 		return quantidade;
 	}
-	
-	public Calendar getData() {
-		return data;
-	}
-	
+		
 	public double getVolume() {
 		return preco * quantidade;
 	}
 
+	public Calendar getData() {
+		return (Calendar) this.data.clone();
+		}
 	
 	
 
